d5dca728818b1c954bdb96d813ed0826e2f0743b
manasvijain06 <manasvijain61104@gmail.com>
2026-03-20 16:45:20 +0530

    Implement grade logic with sub failure and attendance condition

--- frontend/js/manasvi_student_analyzer.js
+++ frontend/js/manasvi_student_analyzer.js
@@ -117,3 +117,68 @@ function getTopper(students) {
   console.log(`Class Topper: ${topper} with ${highestMarks} marks`);
 }
 getTopper(students);
+
+/*Adding the student data who fails the exam */
+    students.push({
+  name: "Aman",
+  marks: [
+    { subject: "Math", score: 68 },   // FAIL (≤ 40)
+    { subject: "English", score: 60 },
+    { subject: "Science", score: 70 },
+    { subject: "History", score: 65 },
+    { subject: "Computer", score: 75 }
+  ],
+  attendance: 60
+});
+
+students.push({
+  name: "Riya",
+  marks: [
+    { subject: "Math", score: 80 },
+    { subject: "English", score: 85 },
+    { subject: "Science", score: 33 },
+    { subject: "History", score: 88 },
+    { subject: "Computer", score: 90 }
+  ],
+  attendance: 80   // FAIL (attendance < 75)
+});
+
+/*Determining pass/fail status based on marks and attendance */
+students.forEach(function(student) {
+  let total = 0;
+  let hasFailed = false;
+  let failReason = "";
+
+  // calculate total marks and check subject failure
+  student.marks.forEach(function(mark) {
+    total += mark.score;
+
+    // if any subject score ≤ 40 → Fail
+    if (mark.score <= 40 && !hasFailed) {
+      hasFailed = true;
+      failReason = "Failed in " + mark.subject;
+    }
+  });
+
+  let average = total / student.marks.length;
+
+  // check attendance condition
+  if (!hasFailed && student.attendance < 75) {
+    hasFailed = true;
+    failReason = "Low Attendance";
+  }
+
+  // print result
+  if (hasFailed) {
+    console.log(student.name + " Grade: Fail (" + failReason + ")");
+  } else {
+    let grade = "";
+
+    if (average >= 85) grade = "A";
+    else if (average >= 70) grade = "B";
+    else if (average >= 50) grade = "C";
+    else grade = "Fail";
+
+    console.log(student.name + " Grade: " + grade);
+  }
+});
